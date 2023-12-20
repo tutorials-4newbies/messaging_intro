e57d8bb7d00befb8ffb01e677982d2522ee95e4f
alonisser <alonisser@gmail.com>
2023-12-20 12:11:09 +0200

    qos done right

--- messaging_intro/receive.py
+++ messaging_intro/receive.py
@@ -8,15 +8,18 @@ def main():
     channel = connection.channel()
 
     channel.queue_declare(queue='hello')
+    channel.basic_qos(prefetch_count=1)
 
     def callback(ch, method, properties, body):
+        # ch.ack(delivery_tag=method.delivery_tag)
+
         print(f" [x] Received {body.decode()}")
         sleep_time = body.count(b'.')
         print(f"Sleep seconds: {sleep_time}")
         time.sleep(sleep_time)
-        # TODO manual ack ch.ack(delivery_tag=method.delivery_tag)
-
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    #
+    channel.basic_consume(queue='hello', on_message_callback=callback)
     # TODO: remove auto ack and implement manual ack
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
